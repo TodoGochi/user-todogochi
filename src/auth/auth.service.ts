98a04234ab39993fcf3fea95ada33433e4822554
yooseungwan <swan1032@naver.com>
2024-09-04 13:16:22 +0900

    [FIX] Store refresh tokens without hashing for improved simplicity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Config } from 'src/common/environment/config';
-import * as argon2 from 'argon2';
 import { UserService } from 'src/user/user.service';
 import { ApiError } from 'src/common/error/api.error';
 
@@ -37,10 +36,7 @@ export class AuthService {
         },
       ),
     ]);
-    await this.userService.saveRefreshToken(
-      userId,
-      await argon2.hash(refreshToken),
-    );
+    await this.userService.saveRefreshToken(userId, refreshToken);
 
     return {
       accessToken,
@@ -53,11 +49,7 @@ export class AuthService {
     if (!user || !user.refreshToken?.token) {
       throw new ApiError('USER-0003');
     }
-    const refreshTokenMatched = await argon2.verify(
-      user.refreshToken.token,
-      refreshToken,
-    );
-    if (!refreshTokenMatched) {
+    if (user.refreshToken.token !== refreshToken) {
       throw new ApiError('USER-0003');
     }
     const tokens = await this.generateTokens(user.userId, user.email);
